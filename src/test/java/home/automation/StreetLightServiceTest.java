ccb3068a9176896a61fc6cfcd86752f6b9855065
Ivan8B <ivan.bolshakov.89@gmail.com>
2023-08-02 10:40:46 +0300

    Расширены тесты на работу с уличным освещением

--- src/test/java/home/automation/StreetLightServiceTest.java
+++ src/test/java/home/automation/StreetLightServiceTest.java
@@ -25,7 +25,7 @@ public class StreetLightServiceTest extends AbstractTest {
             method.setAccessible(true);
             method.invoke(streetLightService, calendar);
         } catch (Exception e) {
-            throw new RuntimeException("Не удалось вызвать метод по расписанию");
+            throw new RuntimeException("Не удалось вызвать метод управления освещением");
         }
     }
 
@@ -33,7 +33,7 @@ public class StreetLightServiceTest extends AbstractTest {
     @DisplayName("Проверка отключения освещения в светлое время суток")
     void checkDisable() throws ModbusException {
         Calendar dayTime = Calendar.getInstance();
-        dayTime.set(2023, Calendar.JULY, 22, 12, 0);
+        dayTime.set(2023, Calendar.JUNE, 22, 12, 0);
         invokeControlMethod(dayTime);
         Mockito.verify(modbusService, Mockito.times(1))
             .writeCoil(configuration.getAddress(), configuration.getCoil(), false);
@@ -43,9 +43,41 @@ public class StreetLightServiceTest extends AbstractTest {
     @DisplayName("Проверка включения освещения в темное время суток")
     void checkEnable() throws ModbusException {
         Calendar nightTime = Calendar.getInstance();
-        nightTime.set(2023, Calendar.DECEMBER, 22, 0, 0);
+        nightTime.set(2023, Calendar.JUNE, 22, 2, 0);
         invokeControlMethod(nightTime);
         Mockito.verify(modbusService, Mockito.times(1))
             .writeCoil(configuration.getAddress(), configuration.getCoil(), true);
+    }
+
+    @Test
+    @DisplayName("Проверка граничного условия - включения в сумерки вечером")
+    void checkEnableEveningTwilight() throws ModbusException {
+        Calendar dayTimeBefore = Calendar.getInstance();
+        dayTimeBefore.set(2023, Calendar.JUNE, 22, 22, 19);
+        invokeControlMethod(dayTimeBefore);
+        Mockito.verify(modbusService, Mockito.times(1))
+            .writeCoil(configuration.getAddress(), configuration.getCoil(), false);
+
+        Calendar dayTimeAfter = Calendar.getInstance();
+        dayTimeAfter.set(2023, Calendar.JUNE, 22, 22, 21);
+        invokeControlMethod(dayTimeAfter);
+        Mockito.verify(modbusService, Mockito.times(1))
+            .writeCoil(configuration.getAddress(), configuration.getCoil(), true);
+    }
+
+    @Test
+    @DisplayName("Проверка граничного условия - отключения в сумерки утром")
+    void checkDisableMorningTwilight() throws ModbusException {
+        Calendar dayTimeBefore = Calendar.getInstance();
+        dayTimeBefore.set(2023, Calendar.JUNE, 22,    2, 43);
+        invokeControlMethod(dayTimeBefore);
+        Mockito.verify(modbusService, Mockito.times(1))
+            .writeCoil(configuration.getAddress(), configuration.getCoil(), true);
+
+        Calendar dayTimeAfter = Calendar.getInstance();
+        dayTimeAfter.set(2023, Calendar.JUNE, 22,    2, 45);
+        invokeControlMethod(dayTimeAfter);
+        Mockito.verify(modbusService, Mockito.times(1))
+            .writeCoil(configuration.getAddress(), configuration.getCoil(), false);
     }
 }
