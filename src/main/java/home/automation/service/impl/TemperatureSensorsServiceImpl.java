da8c16ebbb653ab72ba0076b09b369d70ebc3252
ivan8b <ivan.bolshakov.89@gmail.com>
2024-03-05 23:06:23 +0300

    Улучшено логирование в дебаге

--- src/main/java/home/automation/service/impl/TemperatureSensorsServiceImpl.java
+++ src/main/java/home/automation/service/impl/TemperatureSensorsServiceImpl.java
@@ -64,6 +64,8 @@ public class TemperatureSensorsServiceImpl implements TemperatureSensorsService 
                             sensor.getRegisterId()
                     );
             if (rawTemperature == TEMPERATURE_SENSOR_BORDER_VALUE) {
+                logger.warn("Ошибка опроса  - температурный сенсор DS18B20 не подключен, регистр {}",
+                        sensor.getRegisterId());
                 throw new ModbusException(
                         "Ошибка опроса  - температурный сенсор DS18B20 не подключен, регистр " +
                                 sensor.getRegisterId());
